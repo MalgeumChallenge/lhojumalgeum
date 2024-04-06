2c6259c3d21ec523f8bd4c65f49809789d8199df
12200877 <12200877@inha.edu>
2024-04-06 14:27:30 +0900

    정동주_20240406_(SIM)

--- 2주차/[CTR]_4회.cpp
+++ 2주차/[CTR]_4회.cpp
@@ -1,44 +1,84 @@
-//https://inha.codetree.ai/missions/5/problems/escaping-carry-2/submissions
-#include <iostream>
-#include <vector>
-#include <algorithm>
+//https://inha.codetree.ai/problems/O-mok/description
+#include<iostream>
 using namespace std;
-
-bool hasCarry(int a, int b, int c) {
-    while (a > 0 || b > 0 || c > 0) {
-        int lastDigitA = a % 10;
-        int lastDigitB = b % 10;
-        int lastDigitC = c % 10;
-        if (lastDigitA + lastDigitB + lastDigitC >= 10) {
-            return true;
-        }
-        a /= 10;
-        b /= 10;
-        c /= 10;
-    }
-    return false;
+int grid[19][19];
+bool isRange(int x, int y) {
+	return (0 <= x && x < 19 && 0 <= y && y < 19);
 }
-
 int main() {
-    int n;
-    cin >> n;
-    vector<int> grid(n);
-    for (int i = 0; i < n; i++) {
-        cin >> grid[i];
-    }
-
-    int result = -1;
-    for (int f = 0; f < n; f++) {
-        for (int s = f + 1; s < n; s++) {
-            for (int t = s + 1; t < n; t++) {
-                if (!hasCarry(grid[f], grid[s], grid[t])) {
-                    int sum = grid[f] + grid[s] + grid[t];
-                    result = max(result, sum);
-                }
-            }
-        }
-    }
-
-    cout << result << endl;
-    return 0;
+	for (int i = 0; i < 19; i++) {
+		for (int j = 0; j < 19; j++) {
+			cin >> grid[i][j];
+		}
+	}
+	for (int i = 0; i < 19; i++) {
+		for (int j = 0; j < 19; j++) {
+			if (grid[i][j] == 0) {
+				// 만일 1이나 2가 아니면 할 필요 없음
+				continue;
+			}
+			int count = 0;
+			for (int k = 1; k < 5; k++) {
+				// i / j를 기준으로 1 ~ 4까지 진행
+				// 가로 파악
+				if (!isRange(i, j + k) || grid[i][j] != grid[i][j + k]) {
+					// 하려는 대상이 범위에 없고 같지 않다면 비교 필요 없음
+					break;
+				}
+				count++;
+			}
+			if (count == 4) {
+				cout << grid[i][j] << endl;
+				cout << i + 1 << ' ' << j + 3;
+				return 0;
+			}
+			count = 0;
+			for (int k = 1; k < 5; k++) {
+				// i / j를 기준으로 1 ~ 4까지 진행
+				// 세로 파악
+				if (!isRange(i + k, j) || grid[i][j] != grid[i + k][j]) {
+					// 하려는 대상이 범위에 없고 같지 않다면 비교 필요 없음
+					break;
+				}
+				count++;
+			}
+			if (count == 4) {
+				cout << grid[i][j] << endl;
+				cout << i + 3 << ' ' << j + 1;
+				return 0;
+			}
+			count = 0;
+			for (int k = 1; k < 5; k++) {
+				// i / j를 기준으로 1 ~ 4까지 진행
+				// 대각선 파악
+				if (!isRange(i + k, j + k) || grid[i][j] != grid[i + k][j + k]) {
+					// 하려는 대상이 범위에 없고 같지 않다면 비교 필요 없음
+					break;
+				}
+				count++;
+			}
+			if (count == 4) {
+				cout << grid[i][j] << endl;
+				cout << i + 3 << ' ' << j + 3;
+				return 0;
+			}
+			count = 0;
+			for (int k = 1; k < 5; k++) {
+				// i / j를 기준으로 1 ~ 4까지 진행
+				// 대각선 파악
+				if (!isRange(i-k, j + k) || grid[i][j] != grid[i-k][j + k]) {
+					// 하려는 대상이 범위에 없고 같지 않다면 비교 필요 없음
+					break;
+				}
+				count++;
+			}
+			if (count == 4) {
+				cout << grid[i][j] << endl;
+				cout << i-1 << ' ' << j + 3;
+				return 0;
+			}
+		}
+	}
+	cout << 0;
+	return 0;
 }
